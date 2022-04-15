93f5f26994deddeded420231fc5687ed702591b0
JJJJJJJJJJJJJJJl <costa.joaom@hotmail.com>
2022-04-15 16:35:38 +0100

    model2 dropout layers removed

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -16,16 +16,12 @@ class Model2(tf.keras.Model):
     def __init__(self, hidden_nodes1, hidden_nodes2, hidden_nodes3):
         super().__init__();
         self.dense1 = tf.keras.layers.Dense(hidden_nodes1, input_shape=(8,), kernel_initializer='normal', activation='relu');
-        self.drop1 = tf.keras.layers.Dropout(0.2);
         self.dense2 = tf.keras.layers.Dense(hidden_nodes2, kernel_initializer='normal', activation='relu');
-        self.drop2 = tf.keras.layers.Dropout(0.2);
         self.dense3 = tf.keras.layers.Dense(hidden_nodes3, kernel_initializer='normal', activation='relu');
         self.mouth = tf.keras.layers.Dense(1, kernel_initializer='normal', activation='linear');
 
     def call(self, input):
         layer = self.dense1(input);
-        layer = self.drop1(layer);
         layer = self.dense2(layer);
-        layer = self.drop2(layer);
         layer = self.dense3(layer);
         return self.mouth(layer); 
